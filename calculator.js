bf6e2708bf39b96650b022887c7427a826be3b7d
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-21 09:45:09 +0000

    Add button listeners and screen number appending.
    Clicking number buttons now appends the number to the main screen area,
    unless the screen is showing 0, in which case it is replaced by the first
    number clicked.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,42 @@
 const footer = document.getElementById("footer");
+const screenTop = document.getElementById("screenTop");
+const screenMain = document.getElementById("screenMain");
+const numButtons = document.querySelectorAll(".numButton")
+
+
+numButtons.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        screenMain.innerText === "0" ?
+        screenMain.innerText = `${btn.innerText}` :
+        screenMain.append(`${btn.innerText}`);
+    })
+})
+
+console.log(numButtons);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 footer.innerHTML = `<a style="color:black; text-decoration:none" href="https://github.com/KHetherington89/calculator">Copyright © ${new Date().getFullYear()} KHetherington89 
 <i class="fa fa-github" style="font-size:24p; color:black"></i></a>`;
