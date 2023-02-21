9c1dde321a469a0e440bd685b83c175bb95121de
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-21 20:27:49 +0000

    Add functionality to all buttons but equals.
    All buttons now cause interactions with strings which represent, and are displayed
    as required on the upper and lower sections of the screen. All numbers and operators
    are also stored in an array for evaluation when equals is pressed (not yet
    implamented). Contradictory interactions, such as pressing two operators in a row,
    or trying to have multiple decimal points in one number have been filtered out.

--- calculator.js
+++ calculator.js
@@ -1,19 +1,101 @@
 const footer = document.getElementById("footer");
 const screenTop = document.getElementById("screenTop");
 const screenMain = document.getElementById("screenMain");
-const numButtons = document.querySelectorAll(".numButton")
+const numButtons = document.querySelectorAll(".numButton");
+const opButtons = document.querySelectorAll(".opButton");
+const decimal = document.getElementById("decimal");
+const allClear = document.getElementById("allClear");
+const backspace = document.getElementById("backspace");
+const equals = document.getElementById("equals");
 
+
+let rawMath = [];
+let workingString = "0";
+let topString = "0"
+let opLast = true;
+
+screenMain.innerText = workingString;
+screenTop.innerText = topString;
 
 numButtons.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        screenMain.innerText === "0" ?
-        screenMain.innerText = `${btn.innerText}` :
-        screenMain.append(`${btn.innerText}`);
+    btn.addEventListener("click", () => {
+        if((workingString === "0") || (opLast === true)){
+        workingString = `${btn.dataset.shows}`;
+        }
+        else{
+        workingString += (`${btn.innerText}`);
+        } 
+        opLast = false;
+        screenMain.innerText = "";
+        setTimeout(() => screenMain.innerText = workingString, 30);  //Time out just makes the display blink, so that it's obvious a new number has been entered, even if it's the
+        console.log(opLast);                                         // same as what is currently being shown. Shows calculator is "doing something".        
     })
 })
 
-console.log(numButtons);
+opButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(opLast === false){
+            if(workingString.slice(-1) === "."){
+                workingString = workingString.slice(0, -1)
+            }
+            if(topString === "0"){
+                topString = workingString
+            }
+            else{
+                topString += workingString
+            }
+            rawMath.push(workingString);
+            rawMath.push(`${btn.dataset.shows}`);
+            topString += (`${btn.dataset.shows}`);
+            screenTop.innerText = topString;
+            opLast = true;
+        } 
+        console.log(opLast);
+        console.log(rawMath);
+    })
+})
 
+decimal.addEventListener("click", () => {
+    if(((workingString).includes(".")) && (opLast === false)){} //Empty because we're acting on a false result.
+    else if((workingString === "0") || (opLast === true)){
+        workingString = "0.";
+        opLast = false;
+    }
+    else{
+        workingString += (".")
+    }
+    screenMain.innerText = workingString;
+    console.log(opLast);
+})
+
+allClear.addEventListener("click", () => {
+    workingString = "0";
+    topString = "0";
+    rawMath = [];
+    opLast = true;
+    screenMain.innerText = workingString;
+    screenTop.innerText = topString; 
+})
+
+allClear.addEventListener("click", () => {
+    workingString = "0";
+    topString = "0";
+    rawMath = [];
+    opLast = true;
+    screenMain.innerText = workingString;
+    screenTop.innerText = topString; 
+})
+
+backspace.addEventListener("click", () => {
+    workingString = "0";
+    screenMain.textContent = workingString;
+    rawMath.length = rawMath.length -1;
+    topString = rawMath.join("");
+    screenTop.innerText = topString;
+    console.log(rawMath);
+})
+
+console.log(opLast);
 
 
 
